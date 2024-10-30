7168aac0af3f9e6e8076a4b7a3440fb87090a9ea
Jannet <lsslhh1253@gmail.com>
2024-10-30 19:32:43 +0900

    Add files via upload

---
+++ static/script.js
@@ -0,0 +1,19 @@
+let currentIndex = 0;
+const slides = document.querySelectorAll('.background-slide');
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.classList.remove('active');
+        if (i === index) {
+            slide.classList.add('active');
+        }
+    });
+}
+
+function nextSlide() {
+    currentIndex = (currentIndex + 1) % slides.length;
+    showSlide(currentIndex);
+}
+
+setInterval(nextSlide, 3000); // 3초마다 슬라이드 변경
+showSlide(currentIndex);
